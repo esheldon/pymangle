42fbcae838489aa26a601d2d76f3dc0d3cb077ca
Francesco Montesano <franz.bergesund@gmail.com>
2017-03-22 15:10:53 +0100

    test installation and import of pymangle with and without numpy installed

---
+++ tests/test_import.py
@@ -0,0 +1,9 @@
+"test that pymangle is correctly imported"
+
+import pymangle
+
+
+def test_Mangle():
+    '''Check that is imported and that the Mangle class exists'''
+    pymangle.Mangle
+    pymangle.__version__
