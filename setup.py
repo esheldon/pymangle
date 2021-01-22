8ec331f80a5a3670d7bf3138789e1a0608f2a182
Erin Sheldon <erin.sheldon@gmail.com>
2021-01-22 14:24:35 -0500

    move to setuptools and use markdown long desc

--- setup.py
+++ setup.py
@@ -1,43 +1,38 @@
-import distutils
-from distutils.core import setup, Extension, Command
-import os
+import setuptools
 import numpy
 
+sources = [
+    "pymangle/_mangle.c",
+    "pymangle/mangle.c",
+    "pymangle/cap.c",
+    "pymangle/polygon.c",
+    "pymangle/pixel.c",
+    "pymangle/point.c",
+    "pymangle/stack.c",
+    "pymangle/rand.c",
+]
 
-ext=Extension("pymangle._mangle", ["pymangle/_mangle.c",
-                                   "pymangle/mangle.c",
-                                   "pymangle/cap.c",
-                                   "pymangle/polygon.c",
-                                   "pymangle/pixel.c",
-                                   "pymangle/point.c",
-                                   "pymangle/stack.c",
-                                   "pymangle/rand.c"],
-              include_dirs=[numpy.get_include()])
-              
+ext = setuptools.Extension(
+    "pymangle._mangle",
+    sources,
+    include_dirs=[numpy.get_include()],
+)
 
 exec(open('pymangle/version.py').read())
 
-description="Simple python code to read and work with Mangle masks."
+description = "A python code to read and work with Mangle masks."
 
-dname=os.path.dirname(__file__)
-rstname=os.path.join(dname,'README.rst')
-if os.path.exists(rstname):
-    desc_file=rstname
-else:
-    desc_file=os.path.join(dname,'README.md')
+with open('README.md') as fobj:
+    long_description = fobj.read()
 
-with open(desc_file) as fobj:
-    long_description=fobj.read()
-
-
-
-setup(name="pymangle", 
-      packages=['pymangle'],
-      description=description,
-      long_description=long_description,
-      version=__version__,
-      license = "GPL",
-      ext_modules=[ext],
-      include_dirs=numpy.get_include())
-
-
+setuptools.setup(
+    name="pymangle",
+    packages=['pymangle'],
+    description=description,
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=__version__,
+    license="GPL",
+    ext_modules=[ext],
+    include_dirs=numpy.get_include(),
+)
